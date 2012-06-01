3a85a8159d948d19054a504fc16a3584d2a6824b
ogawa <ogawa@MD005-3.local>
2012-06-01 16:38:48 +0900

    ガラケー識別

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,13 @@ class WelcomeController < ApplicationController
   def index
   	feature = ["DoCoMo","UP.Browser","J-PHONE","Vodafone","SoftBank"]
   	@agent = request.headers['User-Agent']
+  	#@testscript = "J-PHONE/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5 "
 
     feature.each do |fe|
-  		if @agent.index(fe)
+    	check =@agent.include?(fe)
+  		if check
   			@you_are = "feature!"
+  			break
   		else
   			@you_are = "smart!"
   		end
