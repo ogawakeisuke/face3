03b8890cad029974454487b937e4399221e21168
ogawa <ogawa@MD005-3.local>
2012-06-01 16:21:13 +0900

    add whoareyou

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+  	feature = ["DoCoMo","UP.Browser","J-PHONE","Vodafone","SoftBank"]
   	@agent = request.headers['User-Agent']
+
+    feature.each do |fe|
+  		if @agent.index(fe)
+  			@you_are = "feature!"
+  		else
+  			@you_are = "smart!"
+  		end
+  	end
   end
 end
